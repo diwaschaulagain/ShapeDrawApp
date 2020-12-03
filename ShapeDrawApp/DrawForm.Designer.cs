142931c6d9db0a2df0bd503d8529abc7ff610205
diwaschaulagain <chaulagaindiwas@yahoo.com>
2020-12-03 20:44:02 +0545

    Added details to form

---
+++ ShapeDrawApp/DrawForm.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace ShapeDrawApp
+{
+    partial class DrawForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlDisplay = new System.Windows.Forms.Panel();
+            this.txtProgramArea = new System.Windows.Forms.TextBox();
+            this.txtCommand = new System.Windows.Forms.TextBox();
+            this.btnPerformAction = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnPColor = new System.Windows.Forms.Button();
+            this.Fill = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pnlDisplay
+            // 
+            this.pnlDisplay.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pnlDisplay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlDisplay.Location = new System.Drawing.Point(12, 12);
+            this.pnlDisplay.Name = "pnlDisplay";
+            this.pnlDisplay.Size = new System.Drawing.Size(465, 246);
+            this.pnlDisplay.TabIndex = 0;
+            // 
+            // txtProgramArea
+            // 
+            this.txtProgramArea.Location = new System.Drawing.Point(490, 15);
+            this.txtProgramArea.Multiline = true;
+            this.txtProgramArea.Name = "txtProgramArea";
+            this.txtProgramArea.Size = new System.Drawing.Size(169, 211);
+            this.txtProgramArea.TabIndex = 2;
+            this.txtProgramArea.TextChanged += new System.EventHandler(this.txtProgramArea_TextChanged);
+            // 
+            // txtCommand
+            // 
+            this.txtCommand.Location = new System.Drawing.Point(26, 282);
+            this.txtCommand.Name = "txtCommand";
+            this.txtCommand.Size = new System.Drawing.Size(116, 20);
+            this.txtCommand.TabIndex = 3;
+            this.txtCommand.TextChanged += new System.EventHandler(this.txtCommand_TextChanged);
+            // 
+            // btnPerformAction
+            // 
+            this.btnPerformAction.Location = new System.Drawing.Point(26, 308);
+            this.btnPerformAction.Name = "btnPerformAction";
+            this.btnPerformAction.Size = new System.Drawing.Size(99, 23);
+            this.btnPerformAction.TabIndex = 4;
+            this.btnPerformAction.Text = "Perform Action";
+            this.btnPerformAction.UseVisualStyleBackColor = true;
+            this.btnPerformAction.Click += new System.EventHandler(this.btnPerformAction_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(293, 294);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Choose pen color";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnPColor
+            // 
+            this.btnPColor.Location = new System.Drawing.Point(453, 294);
+            this.btnPColor.Name = "btnPColor";
+            this.btnPColor.Size = new System.Drawing.Size(57, 23);
+            this.btnPColor.TabIndex = 6;
+            this.btnPColor.Text = "Color";
+            this.btnPColor.UseVisualStyleBackColor = true;
+            // 
+            // Fill
+            // 
+            this.Fill.Location = new System.Drawing.Point(322, 323);
+            this.Fill.Name = "Fill";
+            this.Fill.Size = new System.Drawing.Size(75, 23);
+            this.Fill.TabIndex = 7;
+            this.Fill.Text = "Fill";
+            this.Fill.UseVisualStyleBackColor = true;
+            // 
+            // DrawForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 363);
+            this.Controls.Add(this.Fill);
+            this.Controls.Add(this.btnPColor);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnPerformAction);
+            this.Controls.Add(this.txtCommand);
+            this.Controls.Add(this.txtProgramArea);
+            this.Controls.Add(this.pnlDisplay);
+            this.Name = "DrawForm";
+            this.Text = "DrawForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlDisplay;
+        private System.Windows.Forms.TextBox txtProgramArea;
+        private System.Windows.Forms.TextBox txtCommand;
+        private System.Windows.Forms.Button btnPerformAction;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnPColor;
+        private System.Windows.Forms.Button Fill;
+    }
+}
+
---
+++ ShapeDrawApp/DrawForm.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShapeDrawApp
+{
+    public partial class DrawForm : Form
+    {
+        Graphics g;
+        Pen outline;
+        SolidBrush fill_color;
+        string comd, prgm;
+
+        string cmd1, cmd2, cmd3, cmd4, cmd5, cmd6, cmd7, cmd8, cmd9, cmd10, cmd11;
+        int x, y, num1, num2, num3, num4, num5, num6, num7, num8, num9, num10;
+
+        public DrawForm()
+        {
+            InitializeComponent();
+            g = pnlDisplay.CreateGraphics();
+            outline = new Pen(Color.Red);
+            fill_color = new SolidBrush(Color.Blue);
+        }
+
+        private void btnPerformAction_Click(object sender, EventArgs e)
+        {
+            switch (comd)
+            {
+                case "run":
+                    if (txtProgramArea.Text == "") { MessageBox.Show("The Command Field Cannot be Empty"); }
+                    else
+                    {
+                        String[] spilt = prgm.Split(' ');
+                        foreach (string words in spilt)
+                        {
+                            cmd1 = spilt[0];
+                        }
+                        switch (cmd1)
+                        {
+                            case "circle":
+                                cmd2 = spilt[1];
+                                cmd3 = spilt[2];
+                                cmd4 = spilt[3];
+                                x = Int32.Parse(cmd2);
+                                y = Int32.Parse(cmd3);
+                                num3 = Int32.Parse(cmd4);
+                                g.DrawEllipse(outline, x, y, num3, num3);
+                                g.FillEllipse(fill_color, x, y, num3, num3);
+                                break;
+                            case "square":
+                                cmd2 = spilt[1];
+                                cmd3 = spilt[2];
+                                cmd4 = spilt[3];
+                                x = Int32.Parse(cmd2);
+                                y = Int32.Parse(cmd3);
+                                num3 = Int32.Parse(cmd4);
+                                Console.WriteLine(cmd1);
+                                g.DrawRectangle(outline, x, y, num3, num3);
+                                g.FillRectangle(fill_color, x, y, num3, num3);
+                                break;
+                            case "rectangle":
+                                cmd2 = spilt[1];
+                                cmd3 = spilt[2];
+                                cmd4 = spilt[3];
+                                cmd5 = spilt[4];
+                                x = Int32.Parse(cmd2);
+                                y = Int32.Parse(cmd3);
+                                num3 = Int32.Parse(cmd4);
+                                num4 = Int32.Parse(cmd5);
+                                Console.WriteLine(cmd1);
+                                g.DrawRectangle(outline, x, y, num3, num4);
+                                g.FillRectangle(fill_color, x, y, num3, num4);
+                                break;
+                            case "polygon":
+                                cmd2 = spilt[1];
+                                cmd3 = spilt[2];
+                                cmd4 = spilt[3];
+                                cmd5 = spilt[4];
+                                cmd6 = spilt[5];
+                                cmd7 = spilt[6];
+                                cmd8 = spilt[7];
+                                cmd9 = spilt[8];
+                                cmd10 = spilt[9];
+                                cmd11 = spilt[10];
+                                num1 = Int32.Parse(cmd2);
+                                num2 = Int32.Parse(cmd3);
+                                num3 = Int32.Parse(cmd4);
+                                num4 = Int32.Parse(cmd5);
+                                num5 = Int32.Parse(cmd6);
+                                num6 = Int32.Parse(cmd7);
+                                num7 = Int32.Parse(cmd8);
+                                num8 = Int32.Parse(cmd9);
+                                num9 = Int32.Parse(cmd10);
+                                num10 = Int32.Parse(cmd11);
+                                Point[] points = new Point[5];
+                                points[0] = new Point(num1, num2);
+                                points[1] = new Point(num3, num4);
+                                points[2] = new Point(num5, num6);
+                                points[3] = new Point(num7, num8);
+                                points[4] = new Point(num9, num10);
+                                g.DrawPolygon(outline, points);
+                                g.FillPolygon(fill_color, points);
+                                break;
+                            default:
+                                MessageBox.Show("you have entered wrong syntax or syntax is missing");
+                                break;
+                        }
+                    }
+
+                    break;
+                case "clear":
+                    g.Clear(Color.White);
+                    txtProgramArea.Text = "";
+                    break;
+                default:
+                    MessageBox.Show("Entered Command must be RUN or CLEAR");
+                    break;
+            }
+        }
+
+        private void txtCommand_TextChanged(object sender, EventArgs e)
+        {
+            comd = txtCommand.Text.ToLower();
+        }
+
+        private void txtProgramArea_TextChanged(object sender, EventArgs e)
+        {
+            prgm = txtProgramArea.Text.ToLower(); 
+        }
+    }
+}
